c1c67bb77a4ebe93d3a9fdd4a4723b15605a51dc
Nickkk14 <nichoolas.ryan@gmail.com>
2022-11-10 10:11:05 -0300

    arrumar botão exibir

--- TCC/botaoExibir/assets/js/script.js
+++ TCC/botaoExibir/assets/js/script.js
@@ -1,4 +1,24 @@
-function showHide(id){
-    let conteudo = document.querySelector(id);
-    conteudo.classList.toggle('ativo');
-}
+let boleto = document.getElementsByClassName('boleto');
+let cartao = document.getElementsByClassName('cartao');
+let pix = document.getElementsByClassName('pix');
+
+function showPix() {
+    pix.classList.toggle('ativo');
+
+    if(boleto.classList.contains('ativo') || cartao.classList.contains('ativo')) {
+        boleto.classList.remove('ativo');
+        cartao.classList.remove('ativo');
+    }
+
+    console.log('clicou pix')
+    }
+
+    cartao.addEventListener('click', () => {
+        pix.classList.toggle('ativo')
+    })
+
+    boleto.addEventListener('click', () => {
+        pix.classList.toggle('ativo')
+    })
+
+
